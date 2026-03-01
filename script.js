5bf3c7b03904103c8c97c440ea66c1d4989b5fd5
ldunlay <lindseydunlay@gmail.com>
2026-03-01 16:43:12 -0600

    changed js to IIFE

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 //Reference website: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
 // Also inspired/borrowed from instructor's github (see README)
 
-async function getPopData() {
+(async function getPopData() {
     const response = await fetch("https://api.datausa.io/tesseract/data.jsonrecords?cube=acs_yg_total_population_5&measures=Population&drilldowns=Year")
     const popDataResult = await response.json();
     console.log(popDataResult);
-}
+})()
 
-getPopData();
